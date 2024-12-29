5c4b1dfd6486b2450a5eb6e44c05dbbf6256283e
Ben Logan <1737958+benlogan@users.noreply.github.com>
2024-12-29 15:34:36 +0000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
+strip1on = False 
+strip2on = False
+
+random1 = 0
+score = 0
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip2 = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
+basic.show_number(score)
+
+def on_forever():
+    global random1
+    random1 = randint(0, 3000)
+    basic.pause(random1)
+    strip2.show_color(neopixel.colors(NeoPixelColors.INDIGO))
+    global strip2on
+    strip2on = True
+    print("STRIP 2 TRUE")
+    basic.pause(750)
+    strip2.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    global strip2on
+    strip2on = False
+basic.forever(on_forever)
+
+def on_forever2():
+    print("CHECKING FOR MATCH strip1on : " + strip1on + " strip2on : " + strip2on)
+    if strip1on and strip2on:
+        global score
+        score = score + 1
+        basic.show_number(score)
+        print("MATCH new score : " + str(score))
+    else:
+        pass
+basic.forever(on_forever2)
 
 def on_every_interval():
     strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
-    basic.pause(200)
+    global strip1on
+    strip1on = True
+    print("STRIP 1 TRUE")
+    basic.pause(750)
     strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
-loops.every_interval(10000, on_every_interval)
-
-def on_every_interval2():
-    random1 = randint(0, 3000)
-    basic.pause(random1)
-    strip2.show_color(neopixel.colors(NeoPixelColors.WHITE))
-    basic.pause(200)
-    strip2.show_color(neopixel.colors(NeoPixelColors.BLACK))
-
-loops.every_interval(1000, on_every_interval2)
+    global strip1on
+    strip1on = False
+loops.every_interval(5000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,15 +1,42 @@
+let strip1on = false
+let strip2on = false
 let random1 = 0
+let score = 0
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 let strip2 = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
-loops.everyInterval(10000, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    basic.pause(500)
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-})
-basic.forever(function () {
+basic.showNumber(score)
+basic.forever(function on_forever() {
+    
     random1 = randint(0, 3000)
     basic.pause(random1)
-    strip2.showColor(neopixel.colors(NeoPixelColors.White))
-    basic.pause(500)
+    strip2.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    
+    strip2on = true
+    console.log("STRIP 2 TRUE")
+    basic.pause(750)
     strip2.showColor(neopixel.colors(NeoPixelColors.Black))
+    
+    strip2on = false
+})
+basic.forever(function on_forever2() {
+    console.log("CHECKING FOR MATCH strip1on : " + strip1on + " strip2on : " + strip2on)
+    if (strip1on && strip2on) {
+        
+        score = score + 1
+        basic.showNumber(score)
+        console.log("MATCH new score : " + ("" + score))
+    } else {
+        
+    }
+    
+})
+loops.everyInterval(5000, function on_every_interval() {
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    
+    strip1on = true
+    console.log("STRIP 1 TRUE")
+    basic.pause(750)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    
+    strip1on = false
 })
